c000f345db918f5f1658b901cd0627c1100ecf33
Mohammed-El-kayesh <70601162+Mohammed-El-kayesh@users.noreply.github.com>
2022-08-04 20:53:38 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,68 +1,36 @@
-import 'dart:async';
-
-import 'package:counter/BMI_Calculator.dart';
-import 'package:counter/BMI_result.dart';
-import 'package:counter/counter_screen.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'bloc_observer.dart';
+import 'home_layout.dart';
 
 
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) => runApp(MyApp()));
+void main()
+{
+  Bloc.observer = MyBlocObserver();
+
+  runApp(MyApp());
 }
 
+// Stateless
+// Stateful
 
+// class MyApp
 
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
 
-class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
+
+    Colors.white;
     return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFFFFFFF)),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 5),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                BMIScreen(),
-            )
-        )
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          image: DecorationImage(
-            image: AssetImage('Assets/images/unnamed.png'),
-            fit: BoxFit.contain,
-          ),
-        ),
-
-    ),
-    );
-  }
-}
-
---
+++ new_tasks_screen.dart
@@ -0,0 +1,27 @@
+import 'package:conditional_builder/conditional_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'components.dart';
+import 'cubit.dart';
+import 'states.dart';
+
+
+class NewTasksScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {},
+      builder: (context, state)
+      {
+        var tasks = AppCubit.get(context).newTasks;
+
+        return tasksBuilder(
+          tasks: tasks,
+        );
+      },
+    );
+  }
+}
